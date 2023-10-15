21e95fd80c9c1c8e5d89d720475b10c78636d70c
Shorzinator <shorz2905@gmail.com>
2023-10-15 18:26:54 -0500

    Improving modularity and updating code prediction capability for the app

--- project_scripts/for_app/estimate_missing_features.py
+++ project_scripts/for_app/estimate_missing_features.py
@@ -1,10 +1,70 @@
+import logging
+
 import pandas as pd
 import pickle
 from sklearn.linear_model import Ridge
 
-from project_scripts.modeling.modeling import evaluate_model
+from project_scripts.utility.model_utils import bootstrap_evaluation, kfold_evaluation
 from project_scripts.utility.path_utils import get_path_from_root
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def get_dynamic_feature_sequence(data, input_features):
+    """
+    Return a list of features sorted by their mean absolute correlation with the input features
+
+    :param data: Input data.
+    :param input_features: Input features.
+    :return: Feature sequence in which predictions would be made.
+    """
+
+    correlation = data.corr().abs()
+    mean_correlations = correlation.drop(input_features, axis=1).loc[input_features].mean()
+    sorted_features = mean_correlations.sort_values(ascending=False).index.tolist()
+    return [feature for feature in sorted_features if feature != 'BODYFAT']
+
+
+def train_and_save_model(X, y, feature_name):
+    """
+    Train a model for the given feature and save it.
+    """
+    # Train a Ridge regression model
+    model = Ridge(alpha=0.1, positive=False, solver='auto')
+    rmse_scores, r2_scores = kfold_evaluation(model, X, y)
+
+    # logging.info(f"Predicting {feature_name} using {X.columns.tolist()}")
+    # logging.info(f"RidReg - RMSE (KFold): {rmse_scores.mean()} +/- {rmse_scores.std()}")
+    # logging.info(f"RidReg - R^2 (KFold): {r2_scores.mean()} +/- {r2_scores.std()}\n")
+    model.fit(X, y)
+
+    # Save the trained model
+    with open(f"model_for_{feature_name}.pkl", "wb") as f:
+        pickle.dump(model, f)
+
+
+def main():
+    data = pd.read_csv(get_path_from_root("data", "preprocessed", "preprocessed_data.csv"))
+    input_features = ["ABDOMEN", "BMI", "WRIST"]
+
+    feature_sequence = get_dynamic_feature_sequence(data, input_features)
+
+    for feature in feature_sequence:
+        X = pd.DataFrame(data[input_features])
+        y = pd.DataFrame(data[feature])
+
+        train_and_save_model(X, y, feature)
+
+        # Add the current feature to the input features for the next iteration
+        input_features.append(feature)
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
 # Load the data
 data = pd.read_csv(get_path_from_root("data", "preprocessed", "preprocessed_data.csv"))
 
@@ -16,14 +76,19 @@ input_features = ['ABDOMEN', 'BMI', 'WRIST']
 
 for feature in features_to_estimate:
     # Split data
-    X = data[input_features]
-    y = data[feature]
+    X = pd.DataFrame(data[input_features])
+    y = pd.DataFrame(data[feature])
 
     # Train a linear regression model
     model = Ridge(alpha=0.1, positive=False, solver='auto')
-    evaluate_model(model, X, y, n_split=5, model_name="Ridge Regression")
+    rmse_scores, r2_scores = kfold_evaluation(model, X, y)
+    logging.info(f"Feature: {feature}")
+    logging.info(f"RidReg - RMSE (KFold): {rmse_scores.mean()} +/- {rmse_scores.std()}")
+    logging.info(f"RidReg - R^2 (KFold): {r2_scores.mean()} +/- {r2_scores.std()}\n")
     model.fit(X, y)
 
     # Save the trained model
     with open(f"model_for_{feature}.pkl", "wb") as f:
         pickle.dump(model, f)
+"""
+
